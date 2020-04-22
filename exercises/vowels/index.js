1ada8416490d8d8ccd343c0d29e1a1c3b3df8fbb
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2020-04-22 13:25:44 +0530

    no message

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -6,7 +6,20 @@
 //   vowels('Hi There!') --> 3
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
-
-function vowels(str) {}
-
+//sol-1
+function vowels1(str) {
+  let count=0;
+  const checker = ["a", "e", "i", "o", "u"];
+  for (let char of str.toLowerCase()) {
+    if(checker.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+//sol - 2  using regex
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 module.exports = vowels;
